0ce9d72cca4138d9591adf478d8b58439f326312
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-05-06 15:26:55 +0100

    feat: add verifiable nft section to landing page

---
+++ src/components/sections/on-chain-verified.tsx
@@ -0,0 +1,15 @@
+import { OnChainVerifiedAnimation } from '../../motions/onchain-verified-animation'
+import { Section } from '../ssectional-ctn'
+
+export const OnChainVerified = () => {
+  return (
+    <div>
+      <Section
+        title='On-Chain Verified Organizations'
+        subtitle="Get hired companies that are verified on credenza. Only authenticated organizations can issue credentials or post jobs, so you know you're dealing with legit entities."
+        animation={<OnChainVerifiedAnimation />}
+        isRowReverse={true}
+      />
+    </div>
+  )
+}
---
+++ src/components/sections/verifiable-nft.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Section } from '../ssectional-ctn'
+
+export const VerifiableNft = () => {
+  return (
+    <div>
+      <Section
+        title='Verifiable NFT Credentials'
+        subtitle='Showcase your achievements with tamper-proof digital credentials. Your professional milestones are issued as NFTs, owned by you, and verifiable by anyone—no central authority needed.'
+        animation={<></>}
+      />
+    </div>
+  )
+}
